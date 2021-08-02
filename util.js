3e87644fe93c915b261443d847cc4d34894e0adf
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2021-08-02 11:57:07 +0200

    test

--- util.js
+++ util.js
@@ -137,7 +137,7 @@ function setViewer(id, hasIGC) {
                 chart.cursor.xAxis = dateAxis;
                 
                 chart.events.on("over", function(ev) {
-                    
+                    console.log("+++",ev.target.xAxis);
                     console.log("+++",ev.x);
                 }, this);
             });
