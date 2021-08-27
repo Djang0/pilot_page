45f87e41cddb72e12ccae48099e69ab528dee242
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2021-08-27 08:56:36 +0200

    test

--- util.js
+++ util.js
@@ -46,7 +46,7 @@ function redrawSitesFilter(sites) {
 function setViewer(id, hasIGC) {
     if (hasIGC) {
         var latlngs = []
-        am4core.ready(function() {
+
             var myCollapse = document.getElementById('collapseExample')
             var bsCollapse = new bootstrap.Collapse(myCollapse, {
                 toggle: true
@@ -106,64 +106,82 @@ function setViewer(id, hasIGC) {
                     accessToken: 'pk.eyJ1IjoidXBza3kiLCJhIjoiY2tycWZodGV2MG1oZDJucGZ3MDV5bmNmeCJ9.f0L_kNkjANGRJO9hlpcpvw'
                 }).addTo(mymap);
 
-                // L.easyButton('fas fa-chart-area', function(btn, map) {
-                //     bsCollapse.toggle();
-                // }).addTo(mymap);
-                // Themes begin
-                am4core.useTheme(am4themes_animated);
-                // Themes end
-                // Create chart instance
-                let chart = am4core.create("chartdiv", am4charts.XYChart);
-
-
-                chart.data = alt_data
-
-                // Create axes
-                var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-
-
-                var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-                valueAxis.min = 500
-                // Create series
-                var series = chart.series.push(new am4charts.LineSeries());
-                series.dataFields.valueY = "gpsalt";
-                series.dataFields.dateX = "date";
-                series.strokeWidth = 2;
-                series.minBulletDistance = 10;
-                series.tooltipText = "Alt. GPS : {gpsalt}\nAlt. Baro {pressalt}";
-                series.tooltip.pointerOrientation = "vertical";
-
-
-                // Create series
-                var series2 = chart.series.push(new am4charts.LineSeries());
-                series2.dataFields.valueY = "pressalt";
-                series2.dataFields.dateX = "date";
-                series2.strokeWidth = 2;
-                series2.strokeDasharray = "3,4";
-                series2.stroke = series.stroke;
-
-                // Add cursor
-                chart.cursor = new am4charts.XYCursor();
-                chart.cursor.xAxis = dateAxis;
-
-                //let bullet = series.bullets.push(new am4charts.CircleBullet());
-                //bullet.fillOpacity = 0
-                //bullet.strokeOpacity = 0
-               // console.log('pre')
-               // bullet.events.on("over", function(ev) {
-                 // console.log(chart.data[ev.target.dataItem.dataContext.indix].gpsalt)
-                    //var data_elem = chart.data[ev.target.dataItem.dataContext.indix]
-                    //if (data_elem) {
-                      //  console.log(chart.data[ev.target.dataItem.dataContext.indix].gpsalt)
-
-                        // var newLatLng = new L.LatLng(chart.data[ev.target.dataItem.dataContext.indix].lat, chart.data[ev.target.dataItem.dataContext.indix].lng);
-                        // cloud.setLatLng(newLatLng);
-                    //}
-
-
-                //}, this);
+                Highcharts.chart('container', {
+    chart: {
+      type: 'area'
+    },
+    accessibility: {
+      description: 'Image description: An area chart compares the nuclear stockpiles of the USA and the USSR/Russia between 1945 and 2017. The number of nuclear weapons is plotted on the Y-axis and the years on the X-axis. The chart is interactive, and the year-on-year stockpile levels can be traced for each country. The US has a stockpile of 6 nuclear weapons at the dawn of the nuclear age in 1945. This number has gradually increased to 369 by 1950 when the USSR enters the arms race with 6 weapons. At this point, the US starts to rapidly build its stockpile culminating in 32,040 warheads by 1966 compared to the USSR’s 7,089. From this peak in 1966, the US stockpile gradually decreases as the USSR’s stockpile expands. By 1978 the USSR has closed the nuclear gap at 25,393. The USSR stockpile continues to grow until it reaches a peak of 45,000 in 1986 compared to the US arsenal of 24,401. From 1986, the nuclear stockpiles of both countries start to fall. By 2000, the numbers have fallen to 10,577 and 21,000 for the US and Russia, respectively. The decreases continue until 2017 at which point the US holds 4,018 weapons compared to Russia’s 4,500.'
+    },
+    title: {
+      text: 'US and USSR nuclear stockpiles'
+    },
+    subtitle: {
+      text: 'Sources: <a href="https://thebulletin.org/2006/july/global-nuclear-stockpiles-1945-2006">' +
+        'thebulletin.org</a> & <a href="https://www.armscontrol.org/factsheets/Nuclearweaponswhohaswhat">' +
+        'armscontrol.org</a>'
+    },
+    xAxis: {
+      allowDecimals: false,
+      labels: {
+        formatter: function () {
+          return this.value; // clean, unformatted number for year
+        }
+      },
+      accessibility: {
+        rangeDescription: 'Range: 1940 to 2017.'
+      }
+    },
+    yAxis: {
+      title: {
+        text: 'Nuclear weapon states'
+      },
+      labels: {
+        formatter: function () {
+          return this.value / 1000 + 'k';
+        }
+      }
+    },
+    tooltip: {
+      pointFormat: '{series.name} had stockpiled <b>{point.y:,.0f}</b><br/>warheads in {point.x}'
+    },
+    plotOptions: {
+      area: {
+        pointStart: 1940,
+        marker: {
+          enabled: false,
+          symbol: 'circle',
+          radius: 2,
+          states: {
+            hover: {
+              enabled: true
+            }
+          }
+        }
+      }
+    },
+    series: [{
+      name: 'USA',
+      keys: ['name', 'custom.value', 'y', 'custom.rank','custom.lng'],
+      point: {
+          events: {
+              mouseOver: function() {
+                  console.log(this.custom.lng);
+              }
+          }
+      },
+      data: [
+        [1940,10,10,3,1004],
+        [1945,100,100,4,22],
+        [1950,1000,1000,5,33],
+        [1955,100,100,6,34],
+        [1960,1000,1000,7,77],
+        [1965,10,10,8,78]
+      ]
+    }]
+  });
             });
-        }); // end am4core.ready()
+
     } else {
         $('#mapinsert').html('<H1> There is no IGC data for this flight</H1>');
     }
