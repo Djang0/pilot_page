857387920d589fb442be92bd382f77b94a83ec67
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2021-08-27 09:19:33 +0200

    test

--- util.js
+++ util.js
@@ -114,24 +114,16 @@ function setViewer(id, hasIGC) {
                 accessibility: {
                     description: 'Image description: An area chart compares the nuclear stockpiles of the USA and the USSR/Russia between 1945 and 2017. The number of nuclear weapons is plotted on the Y-axis and the years on the X-axis. The chart is interactive, and the year-on-year stockpile levels can be traced for each country. The US has a stockpile of 6 nuclear weapons at the dawn of the nuclear age in 1945. This number has gradually increased to 369 by 1950 when the USSR enters the arms race with 6 weapons. At this point, the US starts to rapidly build its stockpile culminating in 32,040 warheads by 1966 compared to the USSR’s 7,089. From this peak in 1966, the US stockpile gradually decreases as the USSR’s stockpile expands. By 1978 the USSR has closed the nuclear gap at 25,393. The USSR stockpile continues to grow until it reaches a peak of 45,000 in 1986 compared to the US arsenal of 24,401. From 1986, the nuclear stockpiles of both countries start to fall. By 2000, the numbers have fallen to 10,577 and 21,000 for the US and Russia, respectively. The decreases continue until 2017 at which point the US holds 4,018 weapons compared to Russia’s 4,500.'
                 },
-                title: {
-                    text: 'US and USSR nuclear stockpiles'
-                },
-                subtitle: {
-                    text: 'Sources: <a href="https://thebulletin.org/2006/july/global-nuclear-stockpiles-1945-2006">' +
-                        'thebulletin.org</a> & <a href="https://www.armscontrol.org/factsheets/Nuclearweaponswhohaswhat">' +
-                        'armscontrol.org</a>'
-                },
                 xAxis: {
                     type: 'datetime'
                 },
                 yAxis: {
                     title: {
-                        text: 'Nuclear weapon states'
+                        text: 'altitude'
                     },
                     labels: {
                         formatter: function() {
-                            return this.value / 1000 + 'k';
+                            return this.value + ' m';
                         }
                     }
                 },
@@ -155,7 +147,7 @@ function setViewer(id, hasIGC) {
                 },
                 series: [{
                     name: 'baro',
-                    keys: ['name', 'custom.value', 'y', 'custom.rank', 'custom.lat', 'custom.lng'],
+                    keys: ['name', 'custom.value', 'y', 'custom.lat', 'custom.lng'],
                     point: {
                         events: {
                             mouseOver: function() {
