6ee231ba2f194c1cfcfd17d38bb01fc7f6288353
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2021-10-08 15:34:57 +0200

    test

--- util.js
+++ util.js
@@ -969,7 +969,7 @@ function redrawYearCount(datas) {
 }
 function renderIcons() {
 
-    // Move icon
+    // glide icon
     if (!this.series[0].icon) {
         this.series[0].icon = this.renderer.path(['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8])
             .attr({
@@ -987,7 +987,7 @@ function renderIcons() {
             (this.series[0].points[0].shapeArgs.r - this.series[0].points[0].shapeArgs.innerR) / 2
     );
 
-    // Exercise icon
+    // thermal icon
     if (!this.series[1].icon) {
         this.series[1].icon = this.renderer.path(
             ['M', -8, 0, 'L', 8, 0, 'M', 0, -8, 'L', 8, 0, 0, 8,
@@ -1008,7 +1008,7 @@ function renderIcons() {
             (this.series[1].points[0].shapeArgs.r - this.series[1].points[0].shapeArgs.innerR) / 2
     );
 
-    // Stand icon
+    // right icon
     if (!this.series[2].icon) {
         this.series[2].icon = this.renderer.path(['M', 0, 8, 'L', 0, -8, 'M', -8, 0, 'L', 0, -8, 8, 0])
             .attr({
@@ -1025,6 +1025,24 @@ function renderIcons() {
         this.chartWidth / 2 - 10,
         this.plotHeight / 2 - this.series[2].points[0].shapeArgs.innerR -
             (this.series[2].points[0].shapeArgs.r - this.series[2].points[0].shapeArgs.innerR) / 2
+    );
+    // left icon
+    if (!this.series[3].icon) {
+        this.series[3].icon = this.renderer.path(['M', 0, 8, 'L', -8, 0, 'M', -8, 0, 'L', 0, -8, 8, 0])
+            .attr({
+                stroke: '#303030',
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                'stroke-width': 2,
+                zIndex: 10
+            })
+            .add(this.series[3].group);
+    }
+
+    this.series[3].icon.translate(
+        this.chartWidth / 2 - 10,
+        this.plotHeight / 2 - this.series[3].points[0].shapeArgs.innerR -
+            (this.series[3].points[0].shapeArgs.r - this.series[3].points[0].shapeArgs.innerR) / 2
     );
 }
 function redrawYearCount(datas) {
