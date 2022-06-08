34f048d4a5cea0704a6c3e7d6abda6a42a6fecc3
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2022-06-08 22:21:54 +0200

    1654719714595

--- p/1/util.js
+++ p/1/util.js
@@ -252,12 +252,13 @@ function setViewer(id) {
 function redrawTable(filteredData) {
 
     if ($.fn.dataTable.isDataTable('#flights_table')) {
-        table = $('#flights_table').DataTable();
+        table = $('#flights_table').DataTable({"autoWidth": false});
         table.clear().rows.add(filteredData).draw();
 
     } else {
 
         var table = $('#flights_table').DataTable({
+            "autoWidth": false,
             aaSorting: [
                 [3, 'desc'],
                 [4, 'desc']
